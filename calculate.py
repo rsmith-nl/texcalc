814d1c1747ed632f22cd889688be12924235798c
Roland Smith <rsmith@xs4all.nl>
2013-12-01 20:52:47 +0100

    Made the function names more meaningful.
    - var → variable.
    - eq → equation.
    - Allow for dimensionless variables and equations.

--- calculate.py
+++ calculate.py
@@ -18,40 +18,54 @@ from __future__ import division, print_function
 __version__ = '$Revision$'[11:-2]
 
 
-def var(name, value, unit, plaintext=False):
+def varable(name, value, unit, plaintext=False):
     """Define a variable and print its value.
 
     :param name: Name of the global variable to define
     :param value: Value of the variable. Will be converted to float internally.
-    :param unit: Unit of the variable
+    :param unit: Unit of the variable or None if dimensionless.
     :param plaintext: Use plaintext output. Default is False; output LaTeX.
     """
     cmd = ' '.join([name, '=', 'float({})'.format(value)])
     exec(cmd, globals())
     if plaintext:
-        print('{} = {} {}'.format(name, value, unit))
+        if unit is not None:
+            print('{} = {} {}'.format(name, value, unit))
+        else:
+            print('{} = {}'.format(name, value))
     else:
-        outs = ''.join(['{} = '.format(name), r'\SI{', '{}'.format(value),
-                        r'}{', unit, r'}\\'])
+        if unit is not None:
+            outs = ''.join(['{} = '.format(name), r'\SI{', '{}'.format(value),
+                            r'}{', unit, r'}\\'])
+        else:
+            outs = ''.join(['{} = '.format(name), r'\num{', '{}'.format(value),
+                            r'}\\'])
         print(outs)
 
 
-def eq(name, expr, unit, texexpr=None, plaintext=False):
+def equation(name, expr, unit, texexpr=None, plaintext=False):
     """Print an equation and its resulting value.
 
     :param name: Name of the global result variable to define
-    :param expr: Expression
-    :param unit: Unit of the result
-    :param texexpr: TeX formatted version of the expression
+    :param expr: The expression to evaluate
+    :param unit: Unit of the result or None if dimensionless.
+    :param texexpr: TeX formatted version of the expression.
     :param plaintext: Use plaintext output. Default is False; output LaTeX.
     """
     cmd = ' '.join([name, '=', expr])
     exec(cmd, globals())
     if plaintext:
-        print('{} = {} = {:g} {}'.format(name, expr, eval(name), unit))
+        if unit is not None:
+            print('{} = {} = {:g} {}'.format(name, expr, eval(name), unit))
+        else:
+            print('{} = {} = {:g}'.format(name, expr, eval(name)))
     else:
         if texexpr is None:
             texexpr = expr
-        outs = ''.join(['{} = '.format(name), texexpr, r' = \SI{',
-                        '{}'.format(eval(name)), r'}{', unit, r'}\\'])
+        if unit is not None:
+            outs = ''.join(['{} = '.format(name), texexpr, r' = \SI{',
+                            '{}'.format(eval(name)), r'}{', unit, r'}\\'])
+        else:
+            outs = ''.join(['{} = '.format(name), texexpr, r' = \num{',
+                            '{}'.format(eval(name)), r'}\\'])
         print(outs)
