d0fcc813eed61534c4b2d6692b6459b442575379
Roland Smith <rsmith@xs4all.nl>
2014-07-06 18:39:18 +0200

    Added recognition for math functions.

--- calculate.py
+++ calculate.py
@@ -18,7 +18,7 @@ import ast
 __version__ = '$Revision$'[11:-2]
 
 
-def expression(name, expr, unit=''):
+def expression(name, expr, unit='', fmt=":.2f"):
     """@todo: Docstring for expression
 
     :param name: @todo
@@ -30,85 +30,93 @@ def expression(name, expr, unit=''):
     exec('{} = {}'.format(name, value), globals())
     n = ast.parse(expr)
     if type(n.body[0].value).__name__ == 'Num':
-        print('${}$ = \SI{{{}}}{{{}}}'.format(name, value, unit))
+        el = ['${}$'.format(name), '=']
+        val = ''.join(['\SI{{{', fmt, '}}}', '{{', unit, '}}'])
+        el.append(val.format(value))
     else:
         v = LatexVisitor()
         v.visit(n)
-        print('${} = {}$ = \SI{{{}}}{{{}}}'.format(name, v.astex(),
-                                                   value, unit))
+        el = ['${} = {}$'.format(name, v.astex()), '=']
+        val = ''.join(['\SI{{{', fmt, '}}}', '{{', unit, '}}'])
+        el.append(val.format(value))
+    print(' '.join(el))
 
 
 class LatexVisitor(ast.NodeVisitor):
     """ example recursive visitor """
 
+    _fnames = {'sin': '\\sin', 'cos': '\\cos', 'tan': '\\tan',
+               'asin': '\\arcsin', 'acos': '\\arccos', 'atan': '\\arctan',
+               'sqrt': '\\sqrt', 'log': '\\ln', 'log10': '\\log',
+               'pi': '\\pi'}
+
     def __init__(self):
         """@todo: to be defined """
-        self.texexpr = []
+        self.txtexpr = []
         self.target = None
         ast.NodeVisitor.__init__(self)
 
     def astex(self):
         """Return a TeX mathematical expression
         """
-        return ''.join(self.texexpr)
+        return ''.join(self.txtexpr)
+
+    def generic_visit(self, node):
+        pass
 
     def visit_Module(self, node):
-        """ visit a Module node recursively"""
         self.visit(node.body[0])
 
     def visit_Expr(self, node):
-        """ visit a Module node recursively"""
         self.visit(node.value)
 
     def visit_BinOp(self, node):
-        """ visit a BinOp node"""
         if type(node.op).__name__ == 'Div':
-            self.texexpr.append(r'\frac{')
+            self.txtexpr.append(r'\frac{')
             self.visit(node.left)
-            self.texexpr.append(r'}{')
+            self.txtexpr.append(r'}{')
             self.visit(node.right)
-            self.texexpr.append(r'}')
+            self.txtexpr.append(r'}')
             return
         self.visit(node.left)
         self.visit(node.op)
         self.visit(node.right)
 
-    def generic_visit(self, node):
-        pass
-
     def visit_Name(self, node):
-        """@todo: Docstring for visit_
-
-        :param node: @todo
-        :returns: @todo
-        """
-        self.texexpr.append(node.id)
+        if node.id in LatexVisitor._fnames:
+            self.txtexpr.append(LatexVisitor._fnames[node.id])
+        else:
+            self.txtexpr.append(node.id)
 
     def visit_Num(self, node):
-        """@todo: Docstring for visit_Num
-
-        :param node: @todo
-        :returns: @todo
-        """
-        self.texexpr.append(str(node.n))
+        self.txtexpr.append(str(node.n))
 
     def visit_Add(self, node):
-        self.texexpr.append('+')
+        self.txtexpr.append('+')
 
     def visit_Sub(self, node):
-        self.texexpr.append('-')
+        self.txtexpr.append('-')
 
     def visit_Mult(self, node):
-        self.texexpr.append(r'\cdot ')
+        self.txtexpr.append(r'\cdot ')
 
     def visit_Pow(self, node):
-        self.texexpr.append('^')
+        self.txtexpr.append('^')
+
+    def visit_Call(self, node):
+        self.visit(node.func)
+        self.txtexpr.append('(')
+        for a in node.args:
+            self.visit(a)
+        self.txtexpr.append(')')
 
     def visit_Div(self, node):
         pass
 
 # Tests
 if __name__ == '__main__':
+    from math import sin, pi
+    sin(pi)
     expression('rho_f', '1.62', 'g/cm^3')
     expression('rho_r', '1.1', 'g/cm^3')
     expression('v_f', '0.3')
@@ -117,3 +125,6 @@ if __name__ == '__main__':
     expression('t', 't_f/v_f', 'mm')
     expression('t_r', 't-t_f', 'mm')
     expression('W_r', 't_f/10*(10000*rho_r)', 'g/m^2')
+    expression('D', '3', 'mm')
+    expression('A', 'pi/4*D**2', 'mm^2')
+    expression('s', 'sin(0.2*pi)')
