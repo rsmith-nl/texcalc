16327f4f6c92f2d0963e156087bcf269159b9de4
Roland Smith <rsmith@xs4all.nl>
2015-09-27 16:53:22 +0200

    Add nosetests for texcalc.

---
+++ tests.py
@@ -0,0 +1,64 @@
+# file: tests.py
+# vim:fileencoding=utf-8:ft=python
+#
+# Author: R.F. Smith <rsmith@xs4all.nl>
+# Created: 2015-09-27 15:46:26 +0200
+# Last modified: 2015-09-27 16:51:01 +0200
+#
+# To the extent possible under law, R.F. Smith has waived all copyright and
+# related or neighboring rights to tests.py. This work is published
+# from the Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/
+
+"""Nose tests for texcalc.
+
+Run with e.g “nosetests-3.5 -v tests.py”.
+"""
+
+from texcalc import _LatexVisitor
+import ast
+
+
+def runvisitor(exptxt, result):
+    """Parse the expression, convert to LaTeX and compare.
+
+    Arguments:
+        exptxt: The expression as a string.
+        result: The expected LaTeX formatted string.
+    """
+    exp = ast.parse(exptxt)
+    v = _LatexVisitor()
+    v.visit(exp)
+    print("DEBUG: v.astex() = ", v.astex())
+    assert result == v.astex()
+
+
+def test_simple():
+    """Test simple expressions."""
+    runvisitor('1 + 2', '1+2')
+    runvisitor('1 - 2', '1-2')
+    runvisitor('1*2', '1\cdot 2')
+    runvisitor('1/2', r'\frac{1}{2}')
+
+
+def test_prec():
+    """Tests involving operator precedence."""
+    runvisitor('1/2+3', r'\frac{1}{2}+3')
+    runvisitor('1/(2+3)', r'\frac{1}{2+3}')
+    runvisitor('(2+3)/21', r'\frac{2+3}{21}')
+    runvisitor('2**(3+2)', r'2^{3+2}')
+    runvisitor('2**(3+2/7)', r'2^{3+\frac{2}{7}}')
+    runvisitor('(2+7)*5', r'{\left(2+7\right)}\cdot 5')
+    runvisitor('54**((2+7)*5)', r'54^{{\left(2+7\right)}\cdot 5}')
+
+
+def test_builtins():
+    """Text the handling of built-in functions."""
+    runvisitor('sin(45)/2', r'\frac{\sin(45)}{2}')
+    runvisitor('atan(7)', r'\arctan(7)')
+    runvisitor('log10(74)**2', r'\log(74)^2')
+
+
+def test_greek():
+    """Text the handling of Greek letters."""
+    runvisitor('tau/2', r'\frac{\tau}{2}')
+    runvisitor('alpha**2/7+3', r'\frac{\alpha^2}{7}+3')
